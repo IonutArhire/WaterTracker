dfc1aadf8c63f77bafcd0857e68224c30157b535
IonutArhire <ionut.arhire@info.uaic.ro>
2018-10-06 15:11:46 +0300

    added android back button functionality

--- main.py
+++ main.py
@@ -2,22 +2,33 @@ import kivy
 kivy.require('1.0.6')
 
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
 
 
 
 class WaterTrackerRoot(BoxLayout):
-    pass
-
+    def onBackBtn(self):
+        self.ids.screen_manager.current = 'start_screen'
+        return True
 
 class WaterTrackerApp(App):
+
+    def __init__(self, **kwargs):
+        super(WaterTrackerApp, self).__init__(**kwargs)
+        Window.bind(on_keyboard=self.onBackBtn)
+
     def build(self):
         return WaterTrackerRoot()
 
-    def on_pause(self):
-        '''Not sure what this does
+    def onBackBtn(self, window, key, *args):
+        ''' When the back button is pressed on
+            android, the app should redirect
+            the user to the main menu.
         '''
-        return True
+        if key == 27:
+            return self.root.onBackBtn()
+            
 
 if __name__ in ('__main__', '__android__'):
     WaterTrackerApp().run()
