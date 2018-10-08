c858ab9227562251602a62f7839df6251b02e563
IonutArhire <ionut.arhire@info.uaic.ro>
2018-10-08 15:01:05 +0300

    implemented History Screen

--- main.py
+++ main.py
@@ -1,16 +1,66 @@
 import kivy
-kivy.require('1.0.6')
+kivy.require('1.9.1')
 
 from kivy.app import App
+from kivy.factory import Factory
+from kivy.uix.label import Label
+from kivy.uix.button import Button
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen
+from kivy.properties import ObjectProperty
+from kivy.storage.jsonstore import JsonStore
 
+
+class StatsScreen(Screen):
+
+    def __init__(self, **kwargs):
+        super(StatsScreen, self).__init__(**kwargs)
+        self.add_widget(Factory.StatsScreenLayout())
+
+
+class HistoryScreen(Screen):
+
+    store = JsonStore('storage.json')
+    
+    def __init__(self, **kwargs):
+        super(HistoryScreen, self).__init__(**kwargs)
+        history_screen_layout = Factory.HistoryScreenLayout()
+        self.add_widget(history_screen_layout)
+
+        for key in self.store.keys():
+            entry = Factory.HistoryEntry()
+
+            entry_text = self.get_entry_text(key)
+            entry_content = Factory.HistoryEntryLabel(text=entry_text)
+
+            entry.add_widget(entry_content)
+            history_screen_layout.history_list.add_widget(entry)
+
+    def get_entry_text(self, key):
+        result = ''
+        entity = self.store.get(key)
+
+        for key, val in entity.items():
+            result += key + ': ' + str(val) + '\n'
+
+        return result[:-1]
 
 
 class WaterTrackerRoot(BoxLayout):
+
+    screen_manager = ObjectProperty(None)
+    history_screen = ObjectProperty(None)
+
     def onBackBtn(self):
-        self.ids.screen_manager.current = 'start_screen'
+        self.screen_manager.current = 'start_screen'
         return True
+
+    def goToHistoryScreen(self):
+        self.screen_manager.current = 'history_screen'
+        self.history_screen.initialize()
+
+
 
 class WaterTrackerApp(App):
 
@@ -28,7 +78,7 @@ class WaterTrackerApp(App):
         '''
         if key == 27:
             return self.root.onBackBtn()
-            
+
 
 if __name__ in ('__main__', '__android__'):
     WaterTrackerApp().run()
